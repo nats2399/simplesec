6a03d133824709b6f2d7a7efece724d77f6712e6
mifidoor <60799069+mifidoor@users.noreply.github.com>
2020-04-12 22:28:24 -0500

    View orders

--- routes/orders.js
+++ routes/orders.js
@@ -20,13 +20,25 @@ const transporter = nodemailer.createTransport({
 
 /* GET View orders page. */
 router.get('/vOrders', function(req, res, next) {
-  res.render('vOrders', { title: 'View Orders' });
+  res.render('vOrders', { title: 'View Order' });
 });
 
 router.get('/getOrders', function(request, response) {
-  var email = request.session.username;
+  var email = '';
+  var orderStatus = '';
+  if( request.session.role=='Employee')
+    {
+      email = request.session.username;
+    }
+    if( request.session.role=='Supervisor'){
+      orderStatus = 'Submitted';
+    } 
+    if( request.session.role=='ordersupervisor'){
+      orderStatus = 'Approved';
+    }  
+  
   var orderid='';
-	let sql = 'call FEATCH_ORDER_DETAILS("'+orderid+'","'+email+'")';
+	let sql = 'call FEATCH_ORDER_DETAILS("'+orderid+'","'+email+'","'+orderStatus+'")';
   mysqlconnection.query(sql, function(err, results, fields) {
     if(!err)
       {
@@ -53,11 +65,11 @@ router.get('/getOrders', function(request, response) {
             orderlist[orderid]=singleOrder;
           }     
         });
-        console.log(orderlist);  
-        response.render('vOrders', { title: 'View Orders',orderdetails: orderlist});
+        
+        response.render('vOrders', { title: 'View Order',orderdetails: orderlist, session: request.session});
         //response.redirect('/orders/vOrders');
       } else {
-        response.send('No Data found');
+        response.render('vOrders', { title: 'No Orders Found'});
       }			
       response.end();
     }
@@ -68,6 +80,63 @@ router.get('/getOrders', function(request, response) {
   });
 });
 
+/* GET viewOrder page. */
+router.get('/viewOrder', function(request, response) {
+  var orderid = request.query.orderId;
+  console.log(orderid);  
+  var email = '';
+  var orderStatus = '';
+  if( request.session.role=='Employee')
+    {
+      email = request.session.username;
+    }
+    if( request.session.role=='Supervisor'){
+      orderStatus = 'Submitted';
+    } 
+    if( request.session.role=='ordersupervisor'){
+      orderStatus = 'Approved';
+    }  
+	let sql = 'call FEATCH_ORDER_DETAILS("'+orderid+'","'+email+'","'+orderStatus+'")';
+  mysqlconnection.query(sql, function(err, results, fields) {
+    if(!err)
+      {
+      if (results.length > 0) {
+            
+        var orderdetails = (JSON.parse(JSON.stringify(results)))[0];
+        
+        request.session.orderdetails = orderdetails;
+        var orderlist=new Map(),i=0,  orderid=0;
+        var singleOrder = new Array();
+        orderdetails.forEach(order => {
+          if(orderid!=order.oOrderID){
+            i=0;
+            singleOrder =  new Array();
+          }            
+          singleOrder[i++]=order;    
+          
+          
+          if(orderid!=order.oOrderID){
+            
+            orderid=order.oOrderID;
+          }      
+          if(orderid!=0){
+            orderlist[orderid]=singleOrder;
+          }     
+        });
+        
+        response.render('vSingleOrder', { title: 'View Order',orderdetails: orderlist, session: request.session});
+        //response.redirect('/orders/vOrders');
+      } else {
+        response.render('vSingleOrder', { title: 'No Orders Found'});
+      }			
+      response.end();
+    }
+    else{
+      response.send({"ERROR":err});
+        return console.error(err.message);
+    }
+  });
+});
 
 
 
