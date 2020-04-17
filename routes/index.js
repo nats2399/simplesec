59f6c256bd09d955a8d3decc994e80b4ec47052e
Nathalie Ayala <nats2399@users.noreply.github.com>
2020-04-16 20:06:23 -0500

    cancel button add order works

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,27 @@ router.get('/', function(req, res, next) {
 
 /* GET Supervisor home page. */
 router.get('/supervisorIndex', function(req, res, next) {
-  res.render('supervisorIndex', { title: 'Welcome Supervisor', user: req.session.username});
+  res.render('supervisorIndex', { title: 'Welcome Supervisor', user: req.session.username, session: req.session});
 });
 
 /* GET Employee home page. */
 router.get('/employeeIndex', function(req, res, next) {
-  res.render('employeeIndex', { title: 'Welcome Employee', user: req.session.username});
+  res.render('employeeIndex', { title: 'Welcome Employee', user: req.session.username, session: req.session});
 });
 
+/* GET Employee home page. */
+router.get('/dashboard', function(req, res, next) {
+
+  if(req.session.role=='Employee')
+    res.render('employeeIndex', { title: 'Welcome Employee', user: req.session.username, session: req.session});
+  else if(req.session.role=='Supervisor')
+    res.render('employeeIndex', { title: 'Welcome Employee', user: req.session.username, session: req.session});
+  else if(req.session.role=='ordersupervisor')
+    res.render('employeeIndex', { title: 'Welcome Employee', user: req.session.username, session: req.session});
+  else
+    res.redirect('/users/login');
+
+});
 
 
 module.exports = router;
