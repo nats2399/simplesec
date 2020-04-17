9d244b84748df8168ab91557f84789910be1c6b6
Nathalie Ayala <nats2399@users.noreply.github.com>
2020-04-16 20:11:49 -0500

    logout corrected

--- routes/users.js
+++ routes/users.js
@@ -226,4 +226,34 @@ router.post('/login/auth', function(request, response) {
 
 
 
+
+
+
+
+
+
+/* LOGOUT  */
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
